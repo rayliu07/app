5ba5473c5d126283363aab3d11301dc0cb74fbb0
Ray <ray@admins-MacBook-Pro.local>
2025-09-05 17:51:36 -0700

    debugging

--- server/db/pool.js
+++ server/db/pool.js
@@ -5,7 +5,7 @@ dotenv.config();
 const pool = new pg.Pool({
   user: 'postgres',           // your DB username
   password: 'Rayliu07@',      // your DB password (can include @, !, etc.)
-  host: '35.193.50.152',      // your Cloud SQL IP
+  host: '/cloudsql/songsearchapp-471216:us-central1:ss',      // your Cloud SQL IP
   port: 5432,                 // default PostgreSQL port
   database: 'ss', 
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
