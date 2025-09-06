40d5d8f621df62efe76bb0aa3c9a3d2cd17579a2
Ray <ray@admins-MacBook-Pro.local>
2025-09-05 18:06:23 -0700

    debugging

--- server/db/pool.js
+++ server/db/pool.js
@@ -5,7 +5,7 @@ dotenv.config();
 const pool = new pg.Pool({
   user: 'ss_j5hw_user',           // your DB username
   password: 'Ou5Qa22hyQQdVPdhlupAVqsvYyDCLe8A',      // your DB password (can include @, !, etc.)
-  host: 'postgresql://ss_j5hw_user:Ou5Qa22hyQQdVPdhlupAVqsvYyDCLe8A@dpg-d2t5e3ruibrs73efkla0-a.oregon-postgres.render.com/ss_j5hw',      // your Cloud SQL IP
+  host: 'dpg-d2t5e3ruibrs73efkla0-a.oregon-postgres.render.com',      // your Cloud SQL IP
   port: 5432,                 // default PostgreSQL port
   database: 'ss_j5hw', 
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
