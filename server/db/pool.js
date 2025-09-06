f7db8f1bea4dbc340b7620dc77b5f363c325d876
Ray <ray@admins-MacBook-Pro.local>
2025-09-05 18:02:07 -0700

    debugging

--- server/db/pool.js
+++ server/db/pool.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new pg.Pool({
-  user: 'postgres',           // your DB username
-  password: 'Rayliu07@',      // your DB password (can include @, !, etc.)
-  host: '/cloudsql/songsearchapp-471216:us-central1:ss',      // your Cloud SQL IP
+  user: 'ss_j5hw_user',           // your DB username
+  password: 'Ou5Qa22hyQQdVPdhlupAVqsvYyDCLe8A',      // your DB password (can include @, !, etc.)
+  host: 'postgresql://ss_j5hw_user:Ou5Qa22hyQQdVPdhlupAVqsvYyDCLe8A@dpg-d2t5e3ruibrs73efkla0-a.oregon-postgres.render.com/ss_j5hw',      // your Cloud SQL IP
   port: 5432,                 // default PostgreSQL port
   database: 'ss', 
-  ssl: false
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 export default pool;
